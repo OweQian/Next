481bf8f2a50b2d5f59ae2096792a906eb41b5570
OweQian <1510106069@qq.com>
2024-06-16 13:58:34 +0800

    feat: templates vs layouts

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,23 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [count, setCount] = useState<number>(0);
   return (
-    <section>
-      <nav>nav</nav>
+    <>
+      <div>
+        <Link href="/dashboard/about">About</Link>
+        <br />
+        <Link href="/dashboard/settings">Settings</Link>
+      </div>
+      <h1>Layout {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
       {children}
-    </section>
+    </>
   );
 }
