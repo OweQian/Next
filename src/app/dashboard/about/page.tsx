6cd4f21f8ea4468e029a0f1905f099903a4a1ea5
OweQian <1510106069@qq.com>
2024-06-16 15:01:06 +0800

    feat: loading

--- src/app/dashboard/about/page.tsx
+++ src/app/dashboard/about/page.tsx
@@ -1,3 +1,12 @@
+import { use } from "react";
+
+async function getData() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  return {
+    message: "Hello, About!",
+  };
+}
 export default function Page() {
-  return <h1>Hello, About!</h1>;
+  const { message } = use(getData());
+  return <h1>{message}</h1>;
 }
